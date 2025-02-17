43f6dc52062cb227a412441ce7899b189c3b12ea
Sushanth Reddy Udumula <sushanthreddyu@gmail.com>
2025-02-18 05:24:45 +0530

    Parsing HTTP methods

--- restful_api/index.js
+++ restful_api/index.js
@@ -17,11 +17,14 @@ const server = http.createServer((req, res) => {
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+    // Get the HTTP method
+    const method = req.method.toLowerCase();
+
     // Send the response 
     res.end('Hello World!!\n');
 
     // Log the request path
-    console.log("Request is received on this path: " + trimmedPath);
+    console.log("Request is received on this path: " + trimmedPath + " with HTTP method:" + method);
 });
 
 // Start the server, and have it listen on port 3000
