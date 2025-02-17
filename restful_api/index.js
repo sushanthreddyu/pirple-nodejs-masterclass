4b45b9609dbc5190623589c6c8b5ca325c7193ca
Sushanth Reddy Udumula <sushanthreddyu@gmail.com>
2025-02-18 04:25:04 +0530

    Parsing Request Paths

---
+++ restful_api/index.js
@@ -0,0 +1,31 @@
+/*
+ * Primary file for the server
+ *
+ */
+
+// Dependencies
+const http = require('http');
+const url = require('url');
+
+// The server should respond to all requests with a string
+const server = http.createServer((req, res) => {
+
+    // Get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Send the response 
+    res.end('Hello World!!\n');
+
+    // Log the request path
+    console.log("Request is received on this path: " + trimmedPath);
+});
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, () => {
+    console.log("The server is now listening on port 3000");
+})
+
