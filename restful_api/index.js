7926d851b5e299c3feb61685b4e07a9372bf51ab
Sushanth Reddy Udumula <sushanthreddyu@gmail.com>
2025-02-19 01:25:44 +0530

    Parsing Payloads

--- restful_api/index.js
+++ restful_api/index.js
@@ -5,7 +5,8 @@
 
 // Dependencies
 const http = require('http');
-const url = require('node:url');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
@@ -26,11 +27,24 @@ const server = http.createServer((req, res) => {
     // Get the headers as an object
     const headers = req.headers
 
-    // Send the response 
-    res.end('Hello World!!\n');
+    // Get the payload, if any
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
 
-    // Log the request path
-    console.log("Request received with these headers : ", headers);
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    })
+
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        // Send the response 
+        res.end('Hello World!!\n');
+
+        // Log the request path
+        console.log("Request received with this payload : ", buffer);
+    })
+
 });
 
 // Start the server, and have it listen on port 3000
